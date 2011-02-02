ccb148f92d57173caca27aeae84bc34a5ef9b6e9
Jorge <jjorge.moura@gmail.com>
2011-02-02 11:40:17 +0000

    First version (initialization) of the Project.
    Signed-off-by: Jorge <jjorge.moura@gmail.com>

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SysControl
+//
+//  Created by Jorge Moura on 2/2/11.
+//  Copyright 2011 Zindon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
